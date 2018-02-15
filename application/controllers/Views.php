200a5a783891db5311f5bad463c9ead43882f288
Alex You <A00933910@ad.bcit.ca>
2018-02-15 15:52:39 -0800

    part 3 doen

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -10,8 +10,42 @@ class Views extends Application
         $this->data['leftside'] = 'by_priority';
         $this->data['rightside'] = 'by_category';
 
+        $this->data['leftside'] = $this->makePrioritizedPanel($tasks);
+        //$this->data['rightside'] = $this->makeCategorizedPanel($tasks);
         $this->render('template_secondary'); 
     }
+    
+function makePrioritizedPanel($tasks) {
+    // extract the undone tasks
+    foreach ($tasks as $task)
+    {
+        if ($task->status != 2)
+            $undone[] = $task;
+    }
+    
+    usort($undone, "orderByPriority");
+    
+    foreach ($undone as $task)
+    $task->priority = $this->app->priority($task->priority);
+    
+    foreach ($undone as $task)
+    $converted[] = (array) $task;
+    $parms = ['display_tasks' => $converted];
+    return $this->parser->parse('by_priority', $parms, true);
+}
+    
+    // return -1, 0, or 1 of $a's priority is higher, equal to, or lower than $b's
+    function orderByPriority($a, $b)
+    {
+        if ($a->priority > $b->priority)
+            return -1;
+        elseif ($a->priority < $b->priority)
+            return 1;
+        else
+            return 0;
+    }
+    
+    
 
 }
 ?>
